1ee7481572c721fff8decfc541b9945e840570b5
Bryce Tucker <bryce.tucker@gmail.com>
2017-07-07 21:29:24 -0700

    site.js changes

--- JobHuntingApp/JobHuntingApp/wwwroot/js/site.js
+++ JobHuntingApp/JobHuntingApp/wwwroot/js/site.js
@@ -1,5 +1,10 @@
 ﻿
 
 //  --------- CoverLetter in Modal on Home/Index page
-
+$(document).ready(function () {
+    $('.cl-save').click(function (event) {
+        var jobID = $(event.target).siblings('.modal-body').children('hidden:first').value;
+        console.log(jobID);
+    });
+});
 
